e53f0e2fed35c28e6e5e31dbb47b9ea2474ed9c2
Oscar <oscar@paramo.dev>
2019-10-11 09:04:14 -0500

    Move stamps reference code to stamps client.

--- StampsService.Tests/StampsClientTests.cs
+++ StampsService.Tests/StampsClientTests.cs
@@ -1,14 +1,23 @@
-using System;
-using Xunit;
+﻿using Xunit;
 
 namespace StampsService.Tests
 {
     public class StampsClientTests
     {
         [Fact]
-        public void Test1()
+        public void IntegrationTest()
         {
+            var credentials = new Credentials
+            {
+                IntegrationID = "dc2cc1f4-7797-4d59-9fa6-bd057006ea67";
+                Username = "OPSC-001";
+                Password = "October2019!";
+            };
 
+            SwsimV84SoapClient client = new SwsimV84SoapClient(SwsimV84SoapClient.EndpointConfiguration.SwsimV84Soap12);
+
+            var authRequest = new AuthenticateUserRequest(credentials);
+            var authResponse = await client.AuthenticateUserAsync(authRequest);
         }
     }
 }
