90cd90a71316a1d0c2644b4956929fe839e1b097
Oscar Paramo <oscar.paramo@me.com>
2019-10-15 08:04:09 -0500

    finish test

--- StampsService.Tests/StampsClientTests.cs
+++ StampsService.Tests/StampsClientTests.cs
@@ -25,21 +25,99 @@ namespace StampsService.Tests
             var authResponse = await client.AuthenticateUserAsync(authRequest);
             var authToken = authResponse.Authenticator;
 
-            // the Stamps API documentation recommends requesting an auth token and updating it:
+            // the Stamps API documentation recommends requesting an auth token first then updating with each subsequent request:
             // https://developer.stamps.com/developer/docs/swsimv84.html#authentication
 
             // build a Rate object that can be used to describe the package and destination
-            var rate = new RateV31();
-            rate.FromZIPCode = "75001";
-            rate.ToZIPCode = "75210";
-            rate.ShipDate = DateTime.Now;
+            var rateQuery = new RateV31();
+            rateQuery.FromZIPCode = "75201";
+            rateQuery.ToZIPCode = "75207";
+            rateQuery.ShipDate = DateTime.Now;
 
-            var getRatesRequest = new GetRatesRequest(authToken, rate);
+            // get rates based off the given rate object
+            var getRatesRequest = new GetRatesRequest(authToken, rateQuery);
             var getRatesResponse = await client.GetRatesAsync(getRatesRequest);
 
-            Console.WriteLine(getRatesResponse);
+            // pick a rate to create indicium later and update the authtoken
+            // a rate is chosen here just for demo purposes
+            var rate = getRatesResponse.Rates[0];
+            authToken = getRatesResponse.Authenticator;
 
+            // create and cleanse a "from" address
+            var fromAddress = new Address();
+            fromAddress.FullName = "John Doe";
+            fromAddress.Address1 = "2200 Ross Ave";
+            fromAddress.City = "Dallas";
+            fromAddress.State = "TX";
+            fromAddress.PostalCode = "75201";
+            fromAddress.PhoneNumber = "9728590000";
 
+            var cleanseFromAddressRequest = new CleanseAddressRequest();
+            cleanseFromAddressRequest.Item = authToken;
+            cleanseFromAddressRequest.Address = fromAddress;
+
+            var cleanseFromAddressResponse = await client.CleanseAddressAsync(cleanseFromAddressRequest);
+            // get the cleansed fromAddress in the response and update the auth token
+            fromAddress = cleanseFromAddressResponse.Address;
+            authToken = cleanseFromAddressResponse.Authenticator;
+
+            // create and cleanse a "to" address
+            var toAddress = new Address();
+            toAddress.FullName = "Jane Doe";
+            toAddress.Address1 = "300 Reunion Boulevard";
+            toAddress.City = "Dallas";
+            toAddress.State = "TX";
+            toAddress.PostalCode = "75207";
+            toAddress.EmailAddress = "oscar@paramo.dev";
+
+            var cleanseToAddressRequest = new CleanseAddressRequest();
+            cleanseToAddressRequest.Item = authToken;
+            cleanseToAddressRequest.Address = toAddress;
+
+            var cleanseToAddressResponse = await client.CleanseAddressAsync(cleanseToAddressRequest);
+            // get the cleansed toAddress in the response and update the auth token
+            toAddress = cleanseToAddressResponse.Address;
+            authToken = cleanseToAddressResponse.Authenticator;
+
+            // create indicium with the given rate and address
+            var createIndiciumRequest = new CreateIndiciumRequest();
+            createIndiciumRequest.Item = authToken;
+            // in a real application, this should be a client generated unique ID for the transaction
+            // if a request fails, this ID could be reused to retry the request without risk of duplicate billing
+            createIndiciumRequest.IntegratorTxID = "demo-transaction-id";
+            createIndiciumRequest.Rate = rate;
+            createIndiciumRequest.From = fromAddress;
+            createIndiciumRequest.To = toAddress;
+
+            var createIndiciumResponse = await client.CreateIndiciumAsync(createIndiciumRequest);
+
+            // get some important shipping details from the createIndiciumResponse and update the auth token
+            var trackingNumber = createIndiciumResponse.TrackingNumber;
+            var url = createIndiciumResponse.URL;
+            var postageBalance = createIndiciumResponse.PostageBalance;
+            var stampsTxID = createIndiciumResponse.StampsTxID;
+            authToken = createIndiciumResponse.Authenticator;
+
+            // print some of the previous details
+            Console.WriteLine($"Tracking number: {trackingNumber}");
+            Console.WriteLine($"Postage URL: {url}");
+            Console.WriteLine($"Available postage balance: {postageBalance.AvailablePostage}");
+            Console.WriteLine($"Stamps transaction ID: {stampsTxID}");
+
+            // create a track shipment object, either trackingNumber or stampsTxID could be used here
+            var trackShipmentRequest = new TrackShipmentRequest(authToken, trackingNumber);
+            var trackShipmentResponse = await client.TrackShipmentAsync(trackShipmentRequest);
+            var trackingEvents = trackShipmentResponse.TrackingEvents;
+
+            // the auth token could also be updated here so it's used in future requests
+            // but that isn't needed for this demo since we generate a new auth token in the beginning
+
+            Console.WriteLine($"Tracking service description: {trackShipmentResponse.ServiceDescription}");
+            foreach (var trackingEvent in trackingEvents)
+            {
+                Console.WriteLine($"Event description: {trackingEvent.Event}");
+                Console.WriteLine($"Event type: {trackingEvent.TrackingEventType}");
+            }
         }
     }
 }
